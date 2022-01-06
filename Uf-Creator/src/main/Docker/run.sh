59f0390146ed63455c257efcf4cb6b0a03a9c329
Jan Marchel <jmarchel@7bulls.com>
2022-01-06 16:50:22 +0100

    init Docker

---
+++ Uf-Creator/src/main/Docker/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/config/wait-for-cdo.sh && java -Djavax.net.ssl.trustStore=/config/common/melodic-truststore.p12 -Djavax.net.ssl.trustStorePassword=melodic -Djavax.net.ssl.trustStoreType=pkcs12 -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/./urandom -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar generator.jar
