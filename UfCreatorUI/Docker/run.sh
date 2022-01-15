4b5d10e1bac8877dda8914232536d2e9354749a1
Jan Marchel <jmarchel@7bulls.com>
2022-01-16 00:30:54 +0100

    updating entrypoint

--- UfCreatorUI/Docker/run.sh
+++ UfCreatorUI/Docker/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+envsubst < /usr/share/nginx/html/assets/appConfig.json > /usr/share/nginx/html/assets/appConfig.json.tmp
+cp /usr/share/nginx/html/assets/appConfig.json.tmp /usr/share/nginx/html/assets/appConfig.json
+
 exec nginx -g 'daemon off;'
